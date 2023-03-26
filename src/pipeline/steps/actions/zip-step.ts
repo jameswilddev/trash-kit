d6ea213fa2ced9280041b5a4a8858f90c51a4436
jameswilddev <jameswilddev@outlook.com>
2023-03-26 16:05:04 +0100

    Switch to a better zip package (fixes #190).

--- src/pipeline/steps/actions/zip-step.ts
+++ src/pipeline/steps/actions/zip-step.ts
@@ -1,5 +1,9 @@
-import * as archiver from "archiver"
-import { BufferListStream } from "bl"
+import * as path from "path"
+import * as os from "os"
+import * as uuid from "uuid"
+import * as fs from "fs"
+import * as childProcess from "child_process";
+import * as _7zipBin from "7zip-bin";
 import StepBase from "../step-base"
 import ActionStepBase from "./action-step-base"
 
@@ -16,20 +20,54 @@ export default class ZipStep extends ActionStepBase {
   }
 
   async execute(): Promise<void> {
-    const archive = archiver(`zip`)
-    const files = this.getFiles()
-    for (const path in files) {
-      archive.append(files[path], { name: path })
-    }
-    this.storeResult(await new Promise<Buffer>((resolve, reject) => {
-      archive.pipe(BufferListStream((error, data) => {
-        if (error) {
-          reject(error)
-        } else {
-          resolve(data)
+    const temporaryDirectory = path.join(os.tmpdir(), uuid.v4())
+
+    try {
+      await Promise.all([
+        fs.promises.mkdir(temporaryDirectory, { recursive: true }),
+        ...Object.entries(this.getFiles()).map(async ([subPath, data]) => {
+          const filePath = path.join(temporaryDirectory, `content`, subPath)
+
+          await fs.promises.mkdir(path.dirname(filePath), { recursive: true })
+          await fs.promises.writeFile(filePath, data)
+        })])
+
+      const targetPath = path.join(temporaryDirectory, `output.zip`)
+
+      await new Promise<void>((resolve, reject) => {
+        childProcess
+          .execFile(_7zipBin.path7za, [
+            `a`,
+            `-mm=Deflate`,
+            `-mfb=258`,
+            `-mpass=15`,
+            `-r`,
+            targetPath,
+            path.join(temporaryDirectory, `content`, `*`),
+          ])
+          .on(`error`, reject)
+          .on(`exit`, (code: number) => {
+            if (code === 0) {
+              resolve();
+            } else {
+              reject(`7za exited with code ${code} (0 expected).`);
+            }
+          })
+      })
+
+      this.storeResult(await fs.promises.readFile(targetPath));
+    } finally {
+      try {
+        await fs.promises.stat(temporaryDirectory)
+      } catch (e) {
+        if (e instanceof Error && 'code' in e && e.code === `ENOENT`) {
+          return
         }
-      }) as unknown as NodeJS.WritableStream)
-      archive.finalize()
-    }))
+
+        throw e
+      }
+
+      await fs.promises.rm(temporaryDirectory, { recursive: true })
+    }
   }
 }
