b5260e9e4607af32000c7a4ff9907445158080d1
jameswilddev <jameswilddev@outlook.com>
2023-03-27 00:46:32 +0100

    Fix a number of ESLint errors.

--- src/engine/src/public-api.ts
+++ src/engine/src/public-api.ts
@@ -1,16 +1,16 @@
-// eslint-disable-line no-unused-vars
-let state: State
+// eslint-disable-next-line @typescript-eslint/no-unused-vars, prefer-const
+let state = undefined as unknown as State
 
-// eslint-disable-line no-unused-vars
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 type Falsy = false | 0 | null | ''
 
-// eslint-disable-line no-unused-vars
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 type Truthiness = 1 | undefined
 
-// eslint-disable-line no-unused-vars
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 type Json = EngineJson
 
-// eslint-disable-line no-unused-vars
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function linearInterpolate (
   from: number,
   to: number,
@@ -19,7 +19,7 @@ function linearInterpolate (
   return from + (to - from) * mixUnitInterval
 }
 
-// eslint-disable-line no-unused-vars
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function dotProduct (
   x1: number,
   y1: number,
@@ -29,7 +29,7 @@ function dotProduct (
   return x1 * x2 + y1 * y2
 }
 
-// eslint-disable-line no-unused-vars
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function magnitudeSquared (
   x: number,
   y: number
@@ -37,7 +37,7 @@ function magnitudeSquared (
   return dotProduct(x, y, x, y)
 }
 
-// eslint-disable-line no-unused-vars
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function magnitude (
   x: number,
   y: number
@@ -45,7 +45,7 @@ function magnitude (
   return Math.sqrt(magnitudeSquared(x, y))
 }
 
-// eslint-disable-line no-unused-vars
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function distanceSquared (
   x1: number,
   y1: number,
@@ -55,7 +55,7 @@ function distanceSquared (
   return magnitudeSquared(x2 - x1, y2 - y1)
 }
 
-// eslint-disable-line no-unused-vars
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function distance (
   x1: number,
   y1: number,
@@ -65,10 +65,10 @@ function distance (
   return magnitude(x2 - x1, y2 - y1)
 }
 
-// eslint-disable-line no-unused-vars
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 type Transform = string
 
-// eslint-disable-line no-unused-vars
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function translate (
   xVirtualPixels: number,
   yVirtualPixels: number
@@ -76,14 +76,14 @@ function translate (
   return `translate(${xVirtualPixels}px,${yVirtualPixels}px)`
 }
 
-// eslint-disable-line no-unused-vars
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function rotate (
   degreesClockwise: number
 ): Transform {
   return `rotate(${degreesClockwise}deg)`
 }
 
-// eslint-disable-line no-unused-vars
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function scale (
   xFactor: number,
   yFactor: number
@@ -91,38 +91,38 @@ function scale (
   return `scale(${xFactor},${yFactor})`
 }
 
-// eslint-disable-line no-unused-vars
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function scaleUniform (
   factor: number
 ): Transform {
   return `scale(${factor})`
 }
 
-// eslint-disable-line no-unused-vars
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 type Filter = string
 
-// eslint-disable-line no-unused-vars
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function soften (
   virtualPixels: number
 ): Filter {
   return `blur(${virtualPixels}px)`
 }
 
-// eslint-disable-line no-unused-vars
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function brightness (
   factor: number
 ): Filter {
   return `brightness(${factor})`
 }
 
-// eslint-disable-line no-unused-vars
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function contrast (
   factor: number
 ): Filter {
   return `contrast(${factor})`
 }
 
-// eslint-disable-line no-unused-vars
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function sharpDropShadow (
   offsetXVirtualPixels: number,
   offsetYVirtualPixels: number,
@@ -137,7 +137,7 @@ function sharpDropShadow (
   return `${output})`
 }
 
-// eslint-disable-line no-unused-vars
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function softDropShadow (
   offsetXVirtualPixels: number,
   offsetYVirtualPixels: number,
@@ -153,49 +153,49 @@ function softDropShadow (
   return `${output})`
 }
 
-// eslint-disable-line no-unused-vars
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function grayscale (
   factor: number
 ): Filter {
   return `grayscale(${factor})`
 }
 
-// eslint-disable-line no-unused-vars
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function hueRotate (
   degrees: number
 ): Filter {
   return `hue-rotate(${degrees}deg)`
 }
 
-// eslint-disable-line no-unused-vars
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function invert (
   factor: number
 ): Filter {
   return `invert(${factor})`
 }
 
-// eslint-disable-line no-unused-vars
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function opacity (
   factor: number
 ): Filter {
   return `opacity(${factor})`
 }
 
-// eslint-disable-line no-unused-vars
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function saturate (
   factor: number
 ): Filter {
   return `saturate(${factor})`
 }
 
-// eslint-disable-line no-unused-vars
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function sepia (
   factor: number
 ): Filter {
   return `sepia(${factor})`
 }
 
-// eslint-disable-line no-unused-vars
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function saveLoadAvailable (): Truthiness {
   try {
     localStorage.setItem(`${gameName}-check`, 'check')
@@ -203,59 +203,59 @@ function saveLoadAvailable (): Truthiness {
   } catch { }
 }
 
-// eslint-disable-line no-unused-vars
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function save<T extends EngineJson> (name: string, content: T): Truthiness {
   return engineStorageSave(`custom-${name}`, content)
 }
 
-// eslint-disable-line no-unused-vars
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function load<T extends EngineJson> (name: string): null | T {
   return engineStorageLoad(`custom-${name}`)
 }
 
-// eslint-disable-line no-unused-vars
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function drop (name: string): Truthiness {
   return engineStorageDrop(`custom-${name}`)
 }
 
-// eslint-disable-line no-unused-vars
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 let root: SVGSVGElement
 
-// eslint-disable-line no-unused-vars
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 type Group = SVGGElement
 
-// eslint-disable-line no-unused-vars
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 type Sprite = SVGUseElement
 
-// eslint-disable-line no-unused-vars
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 type Rectangle = SVGRectElement
 
-// eslint-disable-line no-unused-vars
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 type TextObject = SVGTextElement
 
-// eslint-disable-line no-unused-vars
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 type Parent = SVGSVGElement | Group
 
-// eslint-disable-line no-unused-vars
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 type TransformableChild = Group | Sprite | Rectangle | TextObject
 
-// eslint-disable-line no-unused-vars
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 type FilterableChild = Group | Sprite | Rectangle | TextObject
 
-// eslint-disable-line no-unused-vars
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 type TransformableOrFilterableChild = TransformableChild | FilterableChild
 
-// eslint-disable-line no-unused-vars
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 type ClickableChild = Group | Sprite | Rectangle | TextObject
 
-// eslint-disable-line no-unused-vars
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function group (
   parent: Parent
 ): Group {
   return engineGroup(parent)
 }
 
-// eslint-disable-line no-unused-vars
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function sprite (
   parent: Parent,
   svg: AnySvg
@@ -263,7 +263,7 @@ function sprite (
   return engineSprite(parent, svg)
 }
 
-// eslint-disable-line no-unused-vars
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function rectangle (
   parent: Parent,
   widthVirtualPixels: number,
@@ -273,13 +273,13 @@ function rectangle (
   return engineRectangle(parent, widthVirtualPixels, heightVirtualPixels, fill)
 }
 
-// eslint-disable-line no-unused-vars
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 type TextAnchor = Falsy | 'middle' | 'end'
 
-// eslint-disable-line no-unused-vars
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 type DominantBaseline = Falsy | 'middle' | 'hanging'
 
-// eslint-disable-line no-unused-vars
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function text (
   parent: Parent,
   textContent: string,
@@ -291,7 +291,7 @@ function text (
   return engineText(parent, textContent, fontFamily, color, textAnchor, dominantBaseline)
 }
 
-// eslint-disable-line no-unused-vars
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function transform (
   child: TransformableChild,
   transforms: readonly Transform[]
@@ -299,7 +299,7 @@ function transform (
   engineApplyTransformOrFilter(child, 'transform', transforms)
 }
 
-// eslint-disable-line no-unused-vars
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function filter (
   child: FilterableChild,
   filters: readonly Filter[]
@@ -307,7 +307,7 @@ function filter (
   engineApplyTransformOrFilter(child, 'filter', filters)
 }
 
-// eslint-disable-line no-unused-vars
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function delay (
   child: TransformableOrFilterableChild,
   durationSeconds: number
@@ -315,7 +315,7 @@ function delay (
   engineSetTransition(child, 'jump-end', durationSeconds)
 }
 
-// eslint-disable-line no-unused-vars
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function linear (
   child: TransformableOrFilterableChild,
   durationSeconds: number
@@ -323,7 +323,7 @@ function linear (
   engineSetTransition(child, 'linear', durationSeconds)
 }
 
-// eslint-disable-line no-unused-vars
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function easeOut (
   child: TransformableOrFilterableChild,
   durationSeconds: number
@@ -331,7 +331,7 @@ function easeOut (
   engineSetTransition(child, 'ease-out', durationSeconds)
 }
 
-// eslint-disable-line no-unused-vars
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function easeIn (
   child: TransformableOrFilterableChild,
   durationSeconds: number
@@ -339,7 +339,7 @@ function easeIn (
   engineSetTransition(child, 'ease-in', durationSeconds)
 }
 
-// eslint-disable-line no-unused-vars
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function easeInOut (
   child: TransformableOrFilterableChild,
   durationSeconds: number
@@ -347,7 +347,7 @@ function easeInOut (
   engineSetTransition(child, 'ease-in-out', durationSeconds)
 }
 
-// eslint-disable-line no-unused-vars
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function ease (
   child: TransformableOrFilterableChild,
   durationSeconds: number
@@ -355,7 +355,7 @@ function ease (
   engineSetTransition(child, 'ease', durationSeconds)
 }
 
-// eslint-disable-line no-unused-vars
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function click (
   child: ClickableChild,
   then: () => void
